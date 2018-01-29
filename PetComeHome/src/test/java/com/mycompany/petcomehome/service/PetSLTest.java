e9e476b44f062a4dfa7ca15b71e8302bd0e4463e
MariaOrofino <31407758+MariaOrofino@users.noreply.github.com>
2018-01-29 11:53:09 -0500

    petSL test setup

---
+++ PetComeHome/src/test/java/com/mycompany/petcomehome/service/PetSLTest.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.petcomehome.service;
+
+import com.mycompany.petcomehome.model.Pet;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author n0263892
+ */
+public class PetSLTest {
+
+    public PetSLTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of createPet method, of class PetSL.
+     */
+    @Test
+    public void testCreatePet() {
+    }
+
+    /**
+     * Test of updatePet method, of class PetSL.
+     */
+    @Test
+    public void testUpdatePet() {
+    }
+
+    /**
+     * Test of deletePet method, of class PetSL.
+     */
+    @Test
+    public void testDeletePet() {
+    }
+
+    /**
+     * Test of getPetByPetId method, of class PetSL.
+     */
+    @Test
+    public void testGetPetByPetId() {
+    }
+
+    /**
+     * Test of getPetbyuserId method, of class PetSL.
+     */
+    @Test
+    public void testGetPetbyuserId() {
+    }
+
+    /**
+     * Test of getPetByLocId method, of class PetSL.
+     */
+    @Test
+    public void testGetPetByLocId() {
+    }
+
+    /**
+     * Test of getAllPets method, of class PetSL.
+     */
+    @Test
+    public void testGetAllPets() {
+    }
+
+    public class PetSLImpl implements PetSL {
+
+        public Pet createPet(Pet petId) {
+            return null;
+        }
+
+        public void updatePet(Pet pet) {
+        }
+
+        public void deletePet(int petId) {
+        }
+
+        public Pet getPetByPetId(int petId) {
+            return null;
+        }
+
+        public List<Pet> getPetbyuserId(int userId) {
+            return null;
+        }
+
+        public List<Pet> getPetByLocId(int locId) {
+            return null;
+        }
+
+        public List<Pet> getAllPets() {
+            return null;
+        }
+    }
+
+}
